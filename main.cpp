2b67b37dc9b5b5dbf354c2150ff06f0fd250b9fc
awan <me.awan.es@gmail.com>
2022-08-01 19:04:48 +0700

    Initial Hello World

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    // Hello World
+    cout << "Hello world!" << endl;
+
+    // Assign Variable
+    int num = 15;
+    cout << num << endl;
+    // Inputan
+    cin >> num;
+    cout << "Your input is: " << num << endl;
+    return 0;
+}
